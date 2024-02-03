d5ad93269bd3ca9b3aee2e34eaf462d5c85aedf0
Yildiz Zor Unlu <yildizzor@gmail.com>
2024-02-03 15:49:37 +0100

    sidebar menu for small screens

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,16 +15,15 @@ function Sidebar({ items, selectedItem, setSelectedItem, children }) {
       classNames += " active";
     }
 
-    console.log(selectedItem);
-    console.log(classNames);
     return classNames;
   };
+
   return (
     <div className="container-fluid h-100 p-0">
       <div className="row h-100">
         <div className="col-lg-3 col-12 position-relative">
           <nav className="sidebar">
-            <div className="position-sticky">
+            <div className="position-sticky d-none d-lg-block">
               <div className="list-group list-group-flush mx-3 mt-4">
                 {items.map((element) => {
                   return (
@@ -39,6 +38,23 @@ function Sidebar({ items, selectedItem, setSelectedItem, children }) {
                   );
                 })}
               </div>
+            </div>
+
+            <div className="d-lg-none">
+              <select
+                className="form-control bg-primary text-white"
+                value={selectedItem}
+                onChange={(e) => setSelectedItem(e.target.value)}
+              >
+                {items.map((element) => {
+                  return (
+                    <option key={element.type} value={element.type}>
+                      <FontAwesomeIcon icon={element.icon} className="mr-5" />
+                      <span> {element.type}</span>
+                    </option>
+                  );
+                })}
+              </select>
             </div>
           </nav>
         </div>
